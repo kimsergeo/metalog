7d50fc38c2aa4a92ffb473fcf0a8044aae74e7a0
Gregor Boehl <admin@gregorboehl.com>
2022-01-22 08:41:01 +0100

    avoid 'from metalog import...'

--- metalog/__init__.py
+++ metalog/__init__.py
@@ -1,5 +1,9 @@
 """metalog - the package fits data to metalog distribution and generates samples, quantiles, densities and probabilities based on the fitted distribution."""
 
-__version__ = '0.1.0'
-__author__ = 'Sergey Kim, Reidar Brumer Bratvold <kimsergeo@gmail.com, reidar.bratvold@uis.no>'
+__version__ = "0.1.0"
+__author__ = (
+    "Sergey Kim, Reidar Brumer Bratvold <kimsergeo@gmail.com, reidar.bratvold@uis.no>"
+)
 __all__ = []
+
+from .metalog import *
